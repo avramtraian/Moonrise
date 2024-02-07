f5077b07f320be29ff654f86f676bd26e56c5b6c
Avram Traian <traian.avram1@gmail.com>
2024-02-07 16:36:38 +0200

    AT: Implemented core definitions and types
    This patch implements the core primitive types, move semantics
    functions and various other utilities, as well as core definitions
    regarding current platform and compiler.
    
    The two source files added are meant to be included in every
    other source file in the project.

---
+++ AT/CoreDefines.h
@@ -0,0 +1,109 @@
+/*
+ * Copyright (c) 2024 Traian Avram. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause.
+ */
+
+#pragma once
+
+#ifdef _WIN32
+    #define AT_PLATFORM_WINDOWS 1
+    #ifndef _WIN64
+        #error AT can only be compiled for Windows 64-bit!
+    #endif // _WIN64
+#endif // _WIN32
+
+#if defined(__linux__) || defined(__gnu_linux__)
+    #define AT_PLATFORM_LINUX 1
+#endif // __linux__ || __gnu_linux__
+
+#ifdef __APPLE__
+    #include <TargetConditionals.h>
+    #if TARGET_OS_MAC
+        #define AT_PLATFORM_MACOS 1
+    #else
+        #error The only Apple platform AT supports is macOS!
+    #endif // TARGET_OS_MAC
+#endif // __APPLE__
+
+#ifndef AT_PLATFORM_WINDOWS
+    #define AT_PLATFORM_WINDOWS 0
+#endif // AT_PLATFORM_WINDOWS
+
+#ifndef AT_PLATFORM_LINUX
+    #define AT_PLATFORM_LINUX 0
+#endif // AT_PLATFORM_LINUX
+
+#ifndef AT_PLATFORM_MACOS
+    #define AT_PLATFORM_MACOS 0
+#endif // AT_PLATFORM_MACOS
+
+#if !AT_PLATFORM_WINDOWS && !AT_PLATFORM_LINUX && !AT_PLATFORM_MACOS
+    #error Unknown or unsupported platform!
+#endif // Any desktop supported platform.
+
+#if defined(_MSC_BUILD) && !defined(__clang__)
+    #define AT_COMPILER_MSVC  1
+    #define AT_COMPILER_CLANG 0
+    #define AT_COMPILER_GCC   0
+#endif // _MSC_BUILD && !__clang__
+
+#ifdef __clang__
+    #define AT_COMPILER_MSVC  0
+    #define AT_COMPILER_CLANG 1
+    #define AT_COMPILER_GCC   0
+#endif // __clang__
+
+#if defined(__GNUC__) && !defined(__clang__)
+    #define AT_COMPILER_MSVC  0
+    #define AT_COMPILER_CLANG 0
+    #define AT_COMPILER_GCC   1
+#endif // __GNUC__ && !__clang__
+
+#if !AT_COMPILER_MSVC && !AT_COMPILER_CLANG && !AT_COMPILER_GCC
+    #error Unknown or unsupported compiler!
+#endif // Any supported compiler.
+
+#define NODISCARD    [[nodiscard]]
+#define MAYBE_UNUSED [[maybe_unused]]
+#define LIKELY       [[likely]]
+#define AT_FILE      __FILE__
+#define AT_LINE      __LINE__
+
+#if AT_COMPILER_MSVC
+    #define ALWAYS_INLINE __forceinline
+    #define AT_FUNCTION   __FUNCSIG__
+    #define AT_DEBUGBREAK __debugbreak()
+#elif AT_COMPILER_CLANG
+    #define ALWAYS_INLINE __attribute__((always_inline))
+    #define AT_FUNCTION   __PRETTY_FUNCTION__
+    #define AT_DEBUGBREAK __builtin_trap()
+#elif AT_COMPILER_GCC
+    #define ALWAYS_INLINE __attribute__((always_inline))
+    #define AT_FUNCTION   __PRETTY_FUNCTION__
+    #define AT_DEBUGBREAK __builtin_trap()
+#endif // Compiler switch.
+
+#define AT_STRINGIFY_IMPL(x)      #x
+#define AT_CONCATENATE_IMPL(x, y) x##y
+
+#define AT_STRINGIFY(x)      AT_STRINGIFY_IMPL(x)
+#define AT_CONCATENATE(x, y) AT_CONCATENATE_IMPL(x, y)
+
+#if AT_PLATFORM_WINDOWS
+    #if AT_COMPILER_GCC
+        #define AT_API_SPECIFIER_EXPORT __attribute__((visibility("default")))
+        #define AT_API_SPECIFIER_IMPORT
+    #else
+        #define AT_API_SPECIFIER_EXPORT __declspec(dllexport)
+        #define AT_API_SPECIFIER_IMPORT __declspec(dllimport)
+    #endif // AT_COMPILER_GCC
+#else
+    #define AT_API_SPECIFIER_EXPORT __attribute__((visibility("default")))
+    #define AT_API_SPECIFIER_IMPORT
+#endif // AT_PLATFORM_WINDOWS
+
+#ifdef AT_EXPORT_API
+    #define AT_API AT_API_SPECIFIER_EXPORT
+#else
+    #define AT_API AT_API_SPECIFIER_IMPORT
+#endif // AT_EXPORT_API
---
+++ AT/CoreTypes.h
@@ -0,0 +1,154 @@
+/*
+ * Copyright (c) 2024 Traian Avram. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause.
+ */
+
+#pragma once
+
+#include "AT/CoreDefines.h"
+
+// NOTE: std::initializer_list can't be implemented by us, so we include its header file here in
+//       order to have access to it throughout the entire codebase without including it.
+#include <initializer_list>
+
+namespace Moonrise::AT {
+
+//
+// Unsigned integer primitive types with fixed size.
+// Guaranteed to be the same size on all platforms/architectures.
+//
+using u8 = unsigned char;
+using u16 = unsigned short;
+using u32 = unsigned int;
+using u64 = unsigned long long;
+
+//
+// Signed integer primitive types with fixed size.
+// Guaranteed to be the same size on all platforms/architectures.
+//
+using i8 = signed char;
+using i16 = signed short;
+using i32 = signed int;
+using i64 = signed long long;
+
+// Checks that ensure the sizes of the primitive integer types are as expected.
+// If an mismatch is found, we do not allow the compilation to succeed, as
+// unexpected issues related to alignment or integer overflow may occur.
+static_assert(sizeof(u8) == 1);
+static_assert(sizeof(u16) == 2);
+static_assert(sizeof(u32) == 4);
+static_assert(sizeof(u64) == 8);
+static_assert(sizeof(i8) == 1);
+static_assert(sizeof(i16) == 2);
+static_assert(sizeof(i32) == 4);
+static_assert(sizeof(i64) == 8);
+
+//
+// Integers types that can exactly represent a memory address on the current architecture.
+// Never assume their sizes, as they can vary depending on the platform and architecture.
+//
+using usize = unsigned long long;
+using ssize = signed long long;
+using uintptr = unsigned long long;
+
+constexpr usize invalid_size = static_cast<usize>(-1);
+
+//
+// Integer types that represent a byte and have the allowed access modifiers attached to their name.
+//
+using ReadonlyByte = const u8;
+using WriteonlyByte = u8;
+using ReadWriteByte = u8;
+
+//
+// Types that define a contiguous array of the specified byte types.
+//
+using ReadonlyBytes = ReadonlyByte*;
+using WriteonlyBytes = WriteonlyByte*;
+using ReadWriteBytes = ReadWriteByte*;
+
+//
+// Unsigned integer that represents the numerical value that maps to a specific character in the Unicode scheme.
+// Guaranteed to be the same size (8-bytes) on all platforms/architectures.
+//
+using UnicodeCodepoint = u64;
+constexpr UnicodeCodepoint invalid_unicode_codepoint = static_cast<UnicodeCodepoint>(-1);
+
+#define AT_MAKE_NONCOPYABLE(type_name) \
+    type_name(const type_name&) = delete;    \
+    type_name& operator=(const type_name&) = delete;
+
+#define AT_MAKE_NONMOVABLE(type_name)   \
+    type_name(type_name&&) noexcept = delete; \
+    type_name& operator=(type_name&&) noexcept = delete;
+
+namespace Internal {
+
+template<typename T>
+struct RemoveReference {
+    using Type = T;
+};
+
+template<typename T>
+struct RemoveReference<T&> {
+    using Type = T;
+};
+
+template<typename T>
+struct RemoveReference<T&&> {
+    using Type = T;
+};
+
+template<typename T>
+struct RemoveConst {
+    using Type = T;
+};
+
+template<typename T>
+struct RemoveConst<const T> {
+    using Type = T;
+};
+
+} // namespace Internal
+
+template<typename T>
+using RemoveReference = typename Internal::RemoveReference<T>::Type;
+template<typename T>
+using RemoveConst = typename Internal::RemoveConst<T>::Type;
+
+//
+// The STL equivalent of the move function. Same signature and behaviour.
+//
+template<typename T>
+NODISCARD ALWAYS_INLINE constexpr RemoveReference<T>&& move(T&& instance) noexcept
+{
+    return static_cast<RemoveReference<T>&&>(instance);
+}
+
+//
+// The STL equivalent of the forward function. Same signature and behaviour.
+//
+template<typename T>
+NODISCARD ALWAYS_INLINE constexpr T&& forward(RemoveReference<T>& instance) noexcept
+{
+    return static_cast<T&&>(instance);
+}
+
+//
+// The STL equivalent of the forward function. Same signature and behaviour.
+//
+template<typename T>
+NODISCARD ALWAYS_INLINE constexpr T&& forward(RemoveReference<T>&& instance) noexcept
+{
+    return static_cast<T&&>(instance);
+}
+
+} // namespace Moonrise::AT
+
+namespace Moonrise {
+// NOTE: Writing the AT:: prefix everywhere is very verbose and doesn't have
+//       any benefits, so we just 'using namespace' it.
+//       Objectively, this is not amazing code, but the alternative would be
+//       to 'using AT::ClassName' for every type name, constant or function.
+using namespace AT;
+} // namespace Moonrise
