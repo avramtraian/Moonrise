c3519675ab8a3dc04940cd1eb9c48dd37ccb1565
Avram Traian <traian.avram1@gmail.com>
2024-02-09 17:25:19 +0200

    AT: Added missing AT_API specifier to the Error move constructor

--- AT/Error.h
+++ AT/Error.h
@@ -39,7 +39,7 @@ public:
 
 private:
     Error() = default;
-    Error(Error&& other) noexcept;
+    AT_API Error(Error&& other) noexcept;
     Error& operator=(Error&& other) noexcept = delete;
 
 private:
