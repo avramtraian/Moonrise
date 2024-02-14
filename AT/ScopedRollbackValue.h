0baeb63cf4d22cb521457776984e7644f460bf50
Avram Traian <traian.avram1@gmail.com>
2024-02-14 22:28:31 +0200

    AT: Added a scoped guard that rollbacks a variable to its original value

---
+++ AT/ScopedRollbackValue.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2024 Traian Avram. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause.
+ */
+
+#pragma once
+
+#include "AT/CoreDefines.h"
+#include "AT/CoreTypes.h"
+
+namespace AT {
+
+template<typename T>
+class ScopedValueRollback {
+    AT_MAKE_NONCOPYABLE(ScopedValueRollback);
+    AT_MAKE_NONMOVABLE(ScopedValueRollback);
+
+public:
+    ALWAYS_INLINE ScopedValueRollback(T& variable_to_rollback)
+        : m_variable_to_rollback(variable_to_rollback)
+        , m_initial_value(variable_to_rollback)
+    {}
+
+    ALWAYS_INLINE ~ScopedValueRollback() { m_variable_to_rollback = move(m_initial_value); }
+
+private:
+    T& m_variable_to_rollback;
+    T m_initial_value;
+};
+
+} // namespace AT
