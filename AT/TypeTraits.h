6529e9281da5d15c214d4d8bf381dc97df415a9c
Avram Traian <traian.avram1@gmail.com>
2024-02-18 19:43:32 +0200

    AT: Added a templated TypeTraits structure for computing hashes

---
+++ AT/TypeTraits.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2024 Traian Avram. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause.
+ */
+
+#pragma once
+
+#include "AT/Assertion.h"
+#include "AT/CoreDefines.h"
+#include "AT/CoreTypes.h"
+
+namespace AT {
+
+template<typename T>
+struct TypeTraits {
+    NODISCARD ALWAYS_INLINE static u64 hash(const T&)
+    {
+        AT_ASSERT_NOT_REACHED;
+        return 0;
+    }
+};
+
+template<typename T>
+requires (is_integral<T>)
+struct TypeTraits<T> {
+    NODISCARD ALWAYS_INLINE static u64 hash(const T&) { return 0; }
+};
+
+} // namespace AT
+
+#ifdef AT_INCLUDE_GLOBALLY
+using AT::TypeTraits;
+#endif // AT_INCLUDE_GLOBALLY
