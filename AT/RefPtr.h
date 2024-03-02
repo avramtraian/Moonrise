6b36ab483681f67d391fe22ee54f14a38d2957cc
Avram Traian <traian.avram1@gmail.com>
2024-03-02 19:18:02 +0200

    AT: Implemented a synchronous reference counted smart pointer
    The RefPtr is a templated container that stores and manages an instance
    of a type derived from RefCounted<T>, using automatic reference
    counting. When the reference count hits 0, the object is destroyed and
    its memory is released.
    
    The manipulation of the reference count is *not* performed atomically.
    In the future, there will be an implementation for an AtomicRefCounted
    type that will be thread-safe. ^^

---
+++ AT/RefPtr.h
@@ -0,0 +1,152 @@
+/*
+ * Copyright (c) 2024 Traian Avram. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause.
+ */
+
+#pragma once
+
+#include <AT/Assertion.h>
+#include <AT/Defines.h>
+#include <AT/Types.h>
+
+namespace AT {
+
+template<typename T>
+class RefCounted {
+public:
+    RefCounted()
+        : m_reference_count(0)
+    {}
+    virtual ~RefCounted() = default;
+
+    void increment_reference_count()
+    {
+        AT_ASSERT(m_reference_count > 0);
+
+        // TODO: Ensure that addition would not overflow.
+        ++m_reference_count;
+    }
+
+    bool decrement_reference_count()
+    {
+        AT_ASSERT(m_reference_count > 0);
+
+        if (--m_reference_count == 0) {
+            auto* self = static_cast<T*>(this);
+            delete self;
+            return true;
+        }
+
+        return false;
+    }
+
+    u32 reference_count() const
+    {
+        AT_ASSERT(m_reference_count > 0);
+        return m_reference_count;
+    }
+
+private:
+    u32 m_reference_count;
+};
+
+template<typename T>
+requires (is_derived_from<T, RefCounted<T>>)
+class RefPtr {
+    template<typename Q>
+    friend RefPtr<Q> adopt_ref(Q*);
+
+public:
+    ALWAYS_INLINE RefPtr()
+        : m_instance(nullptr)
+    {}
+
+    ALWAYS_INLINE RefPtr(const RefPtr& other)
+        : m_instance(other.m_instance)
+    {
+        if (m_instance) {
+            static_cast<RefCounted<T>*>(m_instance)->increment_reference_count();
+        }
+    }
+
+    ALWAYS_INLINE RefPtr(RefPtr&& other) noexcept
+        : m_instance(other.m_instance)
+    {
+        other.m_instance = nullptr;
+    }
+
+    ALWAYS_INLINE ~RefPtr()
+    {
+        if (m_instance) {
+            static_cast<RefCounted<T>*>(m_instance)->decrement_reference_count();
+        }
+    }
+
+    ALWAYS_INLINE RefPtr& operator=(const RefPtr& other)
+    {
+        clear();
+        m_instance = other.m_instance;
+        if (m_instance) {
+            static_cast<RefCounted<T>*>(m_instance)->increment_reference_count();
+        }
+        return *this;
+    }
+
+    ALWAYS_INLINE RefPtr& operator=(RefPtr&& other) noexcept
+    {
+        clear();
+        m_instance = other.m_instance;
+        other.m_instance = nullptr;
+        return *this;
+    }
+
+public:
+    NODISCARD ALWAYS_INLINE bool is_valid() const { return (m_instance != nullptr); }
+    NODISCARD ALWAYS_INLINE operator bool() const { return is_valid(); }
+
+    ALWAYS_INLINE void clear()
+    {
+        if (m_instance) {
+            static_cast<RefCounted<T>*>(m_instance)->decrement_reference_count();
+            m_instance = nullptr;
+        }
+    }
+
+private:
+    ALWAYS_INLINE RefPtr(T* pointer_to_adopt)
+        : m_instance(pointer_to_adopt)
+    {
+        if (m_instance) {
+            static_cast<RefCounted<T>*>(m_instance)->increment_reference_count();
+        }
+    }
+
+private:
+    T* m_instance;
+};
+
+template<typename T>
+NODISCARD ALWAYS_INLINE RefPtr<T> adopt_ref(T* pointer_to_adopt)
+{
+    return RefPtr<T>(pointer_to_adopt);
+}
+
+template<typename T>
+NODISCARD ALWAYS_INLINE RefPtr<T> adopt_ref(T& pointer_to_adopt)
+{
+    return adopt_ref(&pointer_to_adopt);
+}
+
+template<typename T, typename... Args>
+NODISCARD ALWAYS_INLINE RefPtr<T> make_ref(Args&&... args)
+{
+    auto* raw_pointer = new T(forward<Args>(args)...);
+    return adopt_ref(raw_pointer);
+}
+
+} // namespace AT
+
+#ifdef AT_INCLUDE_GLOBALLY
+using AT::RefCounted;
+using AT::RefPtr;
+#endif // AT_INCLUDE_GLOBALLY
