fc811ab886423026182c87d307f9275aa3ea3329
Avram Traian <traian.avram1@gmail.com>
2024-05-01 02:25:02 +0300

    AT: Cleaned the Vector container from the ErrorOr architecture
    Moving forward, we want to restrict the usage of the ErrorOr
    architecture only in systems where errors are not actually fatal. There
    is no point in handling out-of-memory errors in an application designed
    for graphics production - there is nothing we can do anyway!
    
    Thus, the Vector API has been streamlined and now doesn't use any of
    the ErrorOr architecture! ^^

--- AT/Vector.h
+++ AT/Vector.h
@@ -7,7 +7,6 @@
 
 #include "AT/Assertion.h"
 #include "AT/Defines.h"
-#include "AT/Error.h"
 #include "AT/Span.h"
 #include "AT/Types.h"
 
@@ -28,9 +27,9 @@ public:
     using ReverseConstIterator = const T*;
 
 public:
-    ALWAYS_INLINE static ErrorOr<Vector> create(const Vector& other)
+    ALWAYS_INLINE static Vector create(const Vector& other)
     {
-        TRY_ASSIGN(auto elements, allocate_memory(other.m_count));
+        T* elements = allocate_memory(other.m_count);
         copy_elements(elements, other.m_elements, other.m_count);
 
         Vector vector;
@@ -40,9 +39,9 @@ public:
         return vector;
     }
 
-    ALWAYS_INLINE static ErrorOr<Vector> create_with_initial_capacity(usize initial_capacity)
+    ALWAYS_INLINE static Vector create_with_initial_capacity(usize initial_capacity)
     {
-        TRY_ASSIGN(auto elements, allocate_memory(initial_capacity));
+        T* elements = allocate_memory(initial_capacity);
 
         Vector vector;
         vector.m_elements = elements;
@@ -51,17 +50,17 @@ public:
         return vector;
     }
 
-    ALWAYS_INLINE static ErrorOr<Vector> create_from_span(Span<const T> element_span)
+    ALWAYS_INLINE static Vector create_from_span(Span<const T> element_span)
     {
-        TRY_ASSIGN(Vector vector, create_with_initial_capacity(element_span.count()));
+        Vector vector = create_with_initial_capacity(element_span.count());
         vector.m_count = element_span.count();
         copy_elements(vector.m_elements, element_span.elements(), element_span.count());
         return vector;
     }
 
-    ALWAYS_INLINE static ErrorOr<Vector> create_filled(usize initial_count)
+    ALWAYS_INLINE static Vector create_filled(usize initial_count)
     {
-        TRY_ASSIGN(Vector vector, create_with_initial_capacity(initial_count));
+        Vector vector = create_with_initial_capacity(initial_count);
         vector.m_count = initial_count;
         for (usize index = 0; index < vector.m_count; ++index) {
             new (vector.m_elements + index) T();
@@ -69,24 +68,14 @@ public:
         return vector;
     }
 
-    ALWAYS_INLINE static ErrorOr<Vector> create_filled(usize initial_count, const T& template_element)
+    ALWAYS_INLINE static Vector create_filled(usize initial_count, const T& template_element)
     {
-        TRY_ASSIGN(Vector vector, create_with_initial_capacity(initial_count));
+        Vector vector = create_with_initial_capacity(initial_count);
         vector.m_count = initial_count;
         for (usize index = 0; index < vector.m_count; ++index) {
             new (vector.m_elements + index) T(template_element);
         }
         return vector;
-    }
-
-    ALWAYS_INLINE static ErrorOr<void> assign(Vector& self, const Vector& other)
-    {
-        self.clear();
-        TRY(self.allocate_new_if_required(other.m_count));
-        self.m_count = other.m_count;
-
-        copy_elements(self.m_elements, other.m_elements, other.m_count);
-        return {};
     }
 
 public:
@@ -97,11 +86,12 @@ public:
     {}
 
     ALWAYS_INLINE Vector(const Vector& other)
+        : m_elements(nullptr)
+        , m_capacity(other.m_count)
+        , m_count(other.m_count)
     {
-        MUST_ASSIGN(Vector copied_vector, create(other));
-        // NOTE: Invoking the move constructor is extremely cheap as there are only a few
-        //       raw pointer assignments and no memory allocation or object initialization.
-        new (this) Vector(move(copied_vector));
+        m_elements = allocate_memory(m_capacity);
+        copy_elements(m_elements, other.m_elements, m_count);
     }
 
     ALWAYS_INLINE Vector(Vector&& other) noexcept
@@ -116,7 +106,10 @@ public:
 
     ALWAYS_INLINE Vector& operator=(const Vector& other)
     {
-        MUST(assign(*this, other));
+        clear();
+        re_allocate_if_required(other.m_count);
+        m_count = other.m_count;
+        copy_elements(m_elements, other.m_elements, m_count);
         return *this;
     }
 
@@ -138,7 +131,7 @@ public:
     ALWAYS_INLINE ~Vector()
     {
         clear();
-        MUST(release_memory(m_elements, m_capacity));
+        release_memory(m_elements, m_capacity);
     }
 
 public:
@@ -215,46 +208,22 @@ public:
 
 public:
     template<typename... Args>
-    ALWAYS_INLINE ErrorOr<void> try_emplace(Args&&... args)
-    {
-        TRY(re_allocate_if_required(m_count + 1));
-        new (m_elements + m_count) T(forward<Args>(args)...);
-        ++m_count;
-        return {};
-    }
-
-    template<typename... Args>
     ALWAYS_INLINE void emplace(Args&&... args)
     {
-        MUST(try_emplace(forward<Args>(args)...));
+        re_allocate_if_required(m_count + 1);
+        new (m_elements + m_count) T(forward<Args>(args)...);
+        ++m_count;
     }
 
-    ALWAYS_INLINE ErrorOr<void> try_add(const T& element)
-    {
-        // NOTE: It will invoke the copy constructor.
-        TRY(try_emplace(element));
-        return {};
-    }
+    ALWAYS_INLINE void add(const T& element) { emplace(element); }
+    ALWAYS_INLINE void add(T&& element) { emplace(move(element)); }
 
-    ALWAYS_INLINE ErrorOr<void> try_add(T&& element)
+    ALWAYS_INLINE void add_span(Span<const T> elements)
     {
-        // NOTE: It will invoke the move constructor.
-        TRY(try_emplace(move(element)));
-        return {};
-    }
-
-    ALWAYS_INLINE void add(const T& element) { MUST(try_emplace(element)); }
-    ALWAYS_INLINE void add(T&& element) { MUST(try_emplace(move(element))); }
-
-    ALWAYS_INLINE ErrorOr<void> try_add_span(Span<const T> elements)
-    {
-        TRY(re_allocate_if_required(m_count + elements.count()));
+        re_allocate_if_required(m_count + elements.count());
         copy_elements(m_elements + m_count, elements.elements(), elements.count());
         m_count += elements.count();
-        return {};
     }
-
-    ALWAYS_INLINE void add_span(Span<const T> elements) { MUST(try_add_span(elements)); }
 
 public:
     ALWAYS_INLINE void remove_last()
@@ -273,18 +242,16 @@ public:
         m_count -= count;
     }
 
-    ALWAYS_INLINE ErrorOr<void> remove_last_and_shrink()
+    ALWAYS_INLINE void remove_last_and_shrink()
     {
         remove_last();
-        TRY(shrink_to_fit());
-        return {};
+        shrink_to_fit();
     }
 
-    ALWAYS_INLINE ErrorOr<void> remove_last_and_shrink(usize count)
+    ALWAYS_INLINE void remove_last_and_shrink(usize count)
     {
         remove_last(count);
-        TRY(shrink_to_fit());
-        return {};
+        shrink_to_fit();
     }
 
 public:
@@ -296,18 +263,16 @@ public:
         m_count = 0;
     }
 
-    ALWAYS_INLINE ErrorOr<void> shrink_to_fit()
+    ALWAYS_INLINE void shrink_to_fit()
     {
-        if (m_capacity > m_count) {
-            TRY(re_allocate_to_fixed(m_count));
-        }
-        return {};
+        if (m_capacity > m_count)
+            re_allocate_to_fixed(m_count);
     }
 
     ALWAYS_INLINE void clear_and_shrink()
     {
         clear();
-        MUST(release_memory(m_elements, m_capacity));
+        release_memory(m_elements, m_capacity);
         m_elements = nullptr;
         m_capacity = 0;
     }
@@ -317,37 +282,30 @@ public:
     // The vector only grows if necessary and there are no guarantees that the new capacity
     // will be exactly equal to required_capacity.
     //
-    ALWAYS_INLINE ErrorOr<void> ensure_capacity(usize required_capacity)
-    {
-        TRY(re_allocate_if_required(required_capacity));
-        return {};
-    }
+    ALWAYS_INLINE void ensure_capacity(usize required_capacity) { re_allocate_if_required(required_capacity); }
 
     //
     // Ensures that the capacity of the vector will be at least equal to the given value.
     // The vector only grows if necessary and if so the new capacity will be equal to fixed_required_capacity.
     //
-    ALWAYS_INLINE ErrorOr<void> ensure_fixed_capacity(usize fixed_required_capacity)
+    ALWAYS_INLINE void ensure_fixed_capacity(usize fixed_required_capacity)
     {
-        if (m_capacity < fixed_required_capacity) {
-            TRY(re_allocate_to_fixed(fixed_required_capacity));
-        }
-        return {};
+        if (m_capacity < fixed_required_capacity)
+            re_allocate_to_fixed(fixed_required_capacity);
     }
 
     //
     // The given capacity must be sufficient to store the elements currently held by the container.
     //
-    ALWAYS_INLINE ErrorOr<void> set_fixed_capacity(usize fixed_capacity)
+    ALWAYS_INLINE void set_fixed_capacity(usize fixed_capacity)
     {
         if (fixed_capacity == m_capacity) {
             // No action needed. Calling re_allocate_to_fixed() in this case would actually trigger an assertion.
-            return {};
+            return;
         }
 
         AT_ASSERT(fixed_capacity >= m_count);
-        TRY(re_allocate_to_fixed(fixed_capacity));
-        return {};
+        re_allocate_to_fixed(fixed_capacity);
     }
 
 public:
@@ -364,26 +322,21 @@ public:
     NODISCARD ALWAYS_INLINE ReverseConstIterator rend() const { return Iterator(m_elements - 1); }
 
 private:
-    NODISCARD ALWAYS_INLINE static ErrorOr<T*> allocate_memory(usize capacity)
+    NODISCARD ALWAYS_INLINE static T* allocate_memory(usize capacity)
     {
-        // TODO: Make sure that capacity * sizeof(T) doesn't overflow.
         void* memory_block = ::operator new(capacity * sizeof(T));
+        AT_ASSERT(memory_block);
 
-        if (memory_block == nullptr) {
-            return Error::OutOfMemory;
-        }
         return reinterpret_cast<T*>(memory_block);
     }
 
-    ALWAYS_INLINE static ErrorOr<void> release_memory(T* elements, usize capacity)
+    ALWAYS_INLINE static void release_memory(T* elements, usize capacity)
     {
         // NOTE: The standard operator delete doesn't need the capacity of the memory block.
         //       However, in future implementations we might switch to a custom memory allocator,
         //       so having this crucial information available out-of-the-box is really handy.
         (void)capacity;
-
         ::operator delete(elements);
-        return {};
     }
 
     ALWAYS_INLINE static void copy_elements(T* destination, const T* source, usize count)
@@ -402,7 +355,6 @@ private:
     }
 
 private:
-    //
     NODISCARD ALWAYS_INLINE usize get_next_capacity(usize required_capacity) const
     {
         const usize geometric_capacity = m_capacity + m_capacity / 2;
@@ -412,7 +364,7 @@ private:
         return geometric_capacity;
     }
 
-    ALWAYS_INLINE ErrorOr<void> allocate_new_to_fixed(usize new_capacity)
+    ALWAYS_INLINE void allocate_new_to_fixed(usize new_capacity)
     {
         // NOTE: These assertions are triggered only by a bug in the Vector implementation.
         //       No user action/command *should* trigger them.
@@ -420,45 +372,41 @@ private:
         AT_ASSERT(new_capacity != m_capacity);
 
         clear();
-        TRY(release_memory(m_elements, m_capacity));
+        release_memory(m_elements, m_capacity);
 
         m_capacity = new_capacity;
-        TRY_ASSIGN(m_elements, allocate_memory(m_capacity));
-        return {};
+        m_elements = allocate_memory(m_capacity);
     }
 
-    ALWAYS_INLINE ErrorOr<void> re_allocate_to_fixed(usize new_capacity)
+    ALWAYS_INLINE void re_allocate_to_fixed(usize new_capacity)
     {
         // NOTE: These assertions are triggered only by a bug in the Vector implementation.
         //       No user action/command *should* trigger them.
         AT_ASSERT(new_capacity >= m_count);
         AT_ASSERT(new_capacity != m_capacity);
 
-        TRY_ASSIGN(auto new_elements, allocate_memory(new_capacity));
+        T* new_elements = allocate_memory(new_capacity);
         move_elements(new_elements, m_elements, m_count);
-        TRY(release_memory(m_elements, m_capacity));
+        release_memory(m_elements, m_capacity);
 
         m_elements = new_elements;
         m_capacity = new_capacity;
-        return {};
     }
 
-    ALWAYS_INLINE ErrorOr<void> re_allocate_if_required(usize required_capacity)
+    ALWAYS_INLINE void re_allocate_if_required(usize required_capacity)
     {
         if (required_capacity > m_capacity) {
             const usize new_capacity = get_next_capacity(required_capacity);
-            TRY(re_allocate_to_fixed(new_capacity));
+            re_allocate_to_fixed(new_capacity);
         }
-        return {};
     }
 
-    ALWAYS_INLINE ErrorOr<void> allocate_new_if_required(usize required_capacity)
+    ALWAYS_INLINE void allocate_new_if_required(usize required_capacity)
     {
         if (required_capacity > m_capacity) {
             const usize new_capacity = get_next_capacity(required_capacity);
-            TRY(allocate_new_to_fixed(new_capacity));
+            allocate_new_to_fixed(new_capacity);
         }
-        return {};
     }
 
 private:
