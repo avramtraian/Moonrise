289f188b0bb823c1306c4fb5065e6f4ddcc9fc23
Avram Traian <traian.avram1@gmail.com>
2024-06-06 23:18:17 +0300

    Core: Using the import/export specifiers defined in the AT framework
    There is no need to redefine them in the MoonCore library when they are
    already present in the AT framework.

--- Moons/MoonCore/Core.h
+++ Moons/MoonCore/Core.h
@@ -5,24 +5,10 @@
 
 #pragma once
 
-#include "AT/Defines.h"
-
-#if AT_PLATFORM_WINDOWS
-    #if AT_COMPILER_MSVC
-        #define CORE_API_SPECIFIER_EXPORT __declspec(dllexport)
-        #define CORE_API_SPECIFIER_IMPORT __declspec(dllimport)
-    #else
-        #define CORE_API_SPECIFIER_EXPORT
-        #define CORE_API_SPECIFIER_IMPORT
-    #endif // AT_COMPILER_MSVC
-#else
-    #define CORE_API_SPECIFIER_EXPORT
-    #define CORE_API_SPECIFIER_IMPORT
-#endif // AT_PLATFORM_WINDOWS
+#include <AT/Defines.h>
 
 #ifdef MOON_CORE_EXPORT_API
-    // Only defined when compiling the actual library.
-    #define CORE_API CORE_API_SPECIFIER_EXPORT
+    #define CORE_API AT_API_SPECIFIER_EXPORT
 #else
-    #define CORE_API CORE_API_SPECIFIER_IMPORT
+    #define CORE_API AT_API_SPECIFIER_IMPORT
 #endif // MOON_CORE_EXPORT_API
