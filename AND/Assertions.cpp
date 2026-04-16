00bfc0c3dee1035cf9a910094f8f922fcf3106b8
Traian Avram <traian.avram1@gmail.com>
2026-04-16 21:36:11 +0300

    AND: Added assertions macros

---
+++ AND/Assertions.cpp
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2024-2026. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause.
+ */
+
+#include "AND/Assertions.h"
+
+namespace AND {
+
+void on_assertion_failed(AssertionKind kind, char const* expression, char const* file, char const* function, int line, char const* message)
+{
+}
+
+} // namespace AND
---
+++ AND/Assertions.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2024-2026. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause.
+ */
+
+#pragma once
+
+#include "AND/Types.h"
+
+namespace AND {
+
+enum class AssertionKind {
+    Assert,
+    AssertNotReached,
+    Panic,
+};
+
+void on_assertion_failed(AssertionKind, char const* expression, char const* file, char const* function, int line, char const* message);
+
+} // namespace AND
+
+#define ASSERT(...)                                                                                                     \
+    do {                                                                                                                \
+        if (!(__VA_ARGS__)) {                                                                                           \
+            ::AND::on_assertion_failed(AssertionKind::Assert, #__VA_ARGS__, AND_FILE, AND_FUNCTION, AND_LINE, nullptr); \
+            AND_DEBUGBREAK;                                                                                             \
+        }                                                                                                               \
+    } while (false)
+
+#define ASSERTF(expression, message)                                                                                   \
+    do {                                                                                                               \
+        if (!(expression)) {                                                                                           \
+            ::AND::on_assertion_failed(AssertionKind::Assert, #expression, AND_FILE, AND_FUNCTION, AND_LINE, message); \
+            AND_DEBUGBREAK;                                                                                            \
+        }                                                                                                              \
+    } while (false)
+
+#define ASSERT_NOT_REACHED                                                                                               \
+    do {                                                                                                                 \
+        ::AND::on_assertion_failed(AssertionKind::AssertNotReached, nullptr, AND_FILE, AND_FUNCTION, AND_LINE, nullptr); \
+        AND_DEBUGBREAK;                                                                                                  \
+    } while (false)
+
+#define ASSERTF_NOT_REACHED(message)                                                                                     \
+    do {                                                                                                                 \
+        ::AND::on_assertion_failed(AssertionKind::AssertNotReached, nullptr, AND_FILE, AND_FUNCTION, AND_LINE, message); \
+        AND_DEBUGBREAK;                                                                                                  \
+    } while (false)
+
+#define PANIC(message)                                                                                        \
+    do {                                                                                                      \
+        ::AND::on_assertion_failed(AssertionKind::Panic, nullptr, AND_FILE, AND_FUNCTION, AND_LINE, message); \
+        AND_DEBUGBREAK;                                                                                       \
+    } while (false)
