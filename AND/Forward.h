56da344bab705f172e2255fefb2de15379bbc8dc
Traian Avram <traian.avram1@gmail.com>
2026-04-16 21:46:53 +0300

    AND: Added forward declarations for string types

---
+++ AND/Forward.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2024-2026. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause.
+ */
+
+#pragma once
+
+namespace AND {
+
+class StringBuilder;
+class Utf16CodePointIterator;
+class Utf16ReverseCodePointIterator;
+class Utf16String;
+class Utf16View;
+class Utf8CodepointIterator;
+class Utf8ReverseCodePointIterator;
+class Utf8String;
+class Utf8View;
+
+} // namespace AND
