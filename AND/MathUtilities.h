3a9a306f3a4884d4883236e5b73558770a76b980
Traian Avram <traian.avram1@gmail.com>
2026-04-18 19:38:17 +0300

    AND: Implemented tne divide_int_up utility math function

--- AND/MathUtilities.h
+++ AND/MathUtilities.h
@@ -30,10 +30,18 @@ NODISCARD ALWAYS_INLINE constexpr T clamp(T value, T min_bound, T max_bound)
     return min(max_bound, max(min_bound, value));
 }
 
+template<typename T>
+requires(is_integer<T>)
+NODISCARD ALWAYS_INLINE constexpr T divide_int_up(T numerator, T denominator)
+{
+    return (numerator + denominator - 1) / denominator;
+}
+
 } // namespace AND
 
 #if AND_INCLUDE_IN_GLOBAL_NAMESPACE
 using AND::clamp;
+using AND::divide_int_up;
 using AND::max;
 using AND::min;
 #endif
