47ec7c83f3e71e189be6d42449c4bf59d9aa4893
Traian Avram <traian.avram1@gmail.com>
2026-04-23 14:04:00 +0300

    Gfx: Fixed integer-conversion compilation warnings generated by MSVC

--- Modules/Gfx/Rect.h
+++ Modules/Gfx/Rect.h
@@ -139,8 +139,8 @@ public:
 
     bool contains(Point<T> point) const
     {
-        bool result_x = m_offset.x <= point.x && point.x < m_offset.x + m_size.width();
-        bool result_y = m_offset.y <= point.y && point.y < m_offset.y + m_size.height();
+        bool result_x = m_offset.x <= point.x && point.x < m_offset.x + static_cast<T>(m_size.width());
+        bool result_y = m_offset.y <= point.y && point.y < m_offset.y + static_cast<T>(m_size.height());
         return result_x && result_y;
     }
 
