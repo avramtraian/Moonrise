ef8c6ec51f77a7a31dbe667a5f17c72b5efc9977
Traian Avram <traian.avram1@gmail.com>
2026-04-25 17:15:53 +0300

    TextEditor: Showcase the HorizontalGroup container

--- Applications/TextEditor/Main.cpp
+++ Applications/TextEditor/Main.cpp
@@ -3,100 +3,44 @@
  * SPDX-License-Identifier: BSD-3-Clause.
  */
 
-#include <AND/Log.h>
 #include <GUI/Application.h>
 #include <GUI/Containers/AlignBox.h>
 #include <GUI/Containers/BorderBox.h>
-#include <GUI/Events/MouseEvent.h>
-#include <GUI/Events/PaintEvent.h>
-#include <GUI/Timer.h>
+#include <GUI/Containers/HorizontalGroup.h>
 #include <GUI/Window.h>
-#include <Gfx/Painter.h>
 
-class TextEditorWidget : public GUI::Widget {
-    GUI_OBJECT(TextEditorWidget, GUI::Widget);
-
-public:
-    virtual void initialize() override
-    {
-        // m_message_timer.initialize(
-        //     TimeDuration::from_seconds(1),
-        //     [&] {
-        //         dbgln("Hello from the timer callback!");
-        //     });
-        m_mouse_position = { 500, 500 };
-    }
-
-    virtual void on_paint_event(GUI::PaintEvent const& event) override
-    {
-        Base::on_paint_event(event);
-        return;
-        Gfx::Painter painter { event.paint_buffer(), m_layout_region };
-
-        auto left = m_layout_region;
-        left.set_width(left.width() / 2);
-
-        auto right = m_layout_region;
-        right.move_x_by(left.width());
-        right.set_width(m_layout_region.width() - left.width());
-
-        auto percentage = static_cast<float>(m_layout_region.height()) / static_cast<float>(m_layout_region.width());
-        percentage = clamp(percentage, 0.0F, 1.0F);
-
-        auto top = left;
-        top.set_height(static_cast<u32>(top.height() * percentage));
-        top.move_y_by(left.height() - top.height());
-
-        painter.fill_rect(left, Gfx::Color::from_rgb(0.1F, 0.2F, 0.8F));
-        painter.fill_rect(right, Gfx::Color::from_rgb(0.8F, 0.3F, 0.2F));
-        painter.fill_rect(top, Gfx::Color::from_rgb(0.3F, 0.9F, 0.2F));
-
-        Gfx::IntRect cursor;
-        cursor.set_size(20, 20);
-        cursor.center_in(m_mouse_position);
-        painter.fill_rect(cursor, Gfx::Color::from_rgb(0.9F, 0.9F, 0.9F));
-    }
-
-    virtual void on_mouse_moved_event(GUI::MouseEvent const& event) override
-    {
-        Base::on_mouse_moved_event(event);
-        m_mouse_position = event.position();
-        schedule_paint_event();
-    }
-
-private:
-    virtual Gfx::IntSize calculate_preferred_size() const override
-    {
-        return { 400, 300 };
-    }
-
-private:
-    GUI::Timer m_message_timer;
-    Gfx::IntPoint m_mouse_position;
-};
-
-int main(int argument_count, char** arguments)
+static NonnullRefPtr<GUI::BorderBox> add_child(NonnullRefPtr<GUI::HorizontalGroup> group)
 {
-    auto application = GUI::Application::construct(argument_count, arguments);
-    auto window = application->add_window<GUI::Window>();
-    auto outer_border = window->set_main_widget<GUI::BorderBox>();
-    auto align_box = outer_border->set_widget<GUI::AlignBox>();
-    auto inner_border = align_box->set_widget<GUI::BorderBox>();
-    auto widget = inner_border->set_widget<TextEditorWidget>();
+    auto child = group->add_widget<GUI::BorderBox>();
+    child->set_border(Gfx::IntBorder::uniform(4));
+    child->set_color(Gfx::Color::from_rgb(0.1F, 0.1F, 0.1F));
+    child->set_background(Gfx::Color::from_rgb(0.15F, 0.15F, 0.15F));
+    group->set_widget_size_policy(child, GUI::SizePolicy::Fill);
+    return child;
+}
 
-    outer_border->set_color(Gfx::Color::from_rgb(0.12F, 0.12F, 0.12F));
-    outer_border->set_background(Gfx::Color::from_rgb(0.20F, 0.20F, 0.20F));
-    outer_border->set_border(Gfx::IntBorder::uniform(4));
+int main(int argc, char** argv)
+{
+    auto app = GUI::Application::construct(argc, argv);
+    auto window = app->add_window<GUI::Window>();
 
-    inner_border->set_color(Gfx::Color::from_rgb(0.12F, 0.12F, 0.12F));
-    inner_border->set_background(Gfx::Color::from_rgb(0.20F, 0.20F, 0.20F));
-    inner_border->set_border(Gfx::IntBorder::uniform(4));
+    auto group = window->set_main_widget<GUI::HorizontalGroup>();
+    group->set_widget_preferred_width(add_child(group), GUI::Length::from_pixels(50));
 
-    align_box->set_preferred_width(GUI::Length::from_pixels(500));
-    align_box->set_preferred_height(GUI::Length::from_percentage(0.5F));
-    align_box->set_alignment(GUI::Alignment::BottomLeft);
-    align_box->set_offset(100, 100);
+    auto align = group->add_widget<GUI::AlignBox>();
+    group->set_widget_size_policy(align, GUI::SizePolicy::Fill);
+
+    auto child = align->set_widget<GUI::BorderBox>();
+    child->set_border(Gfx::IntBorder::uniform(4));
+    child->set_color(Gfx::Color::from_rgb(0.1F, 0.1F, 0.1F));
+    child->set_background(Gfx::Color::from_rgb(0.15F, 0.15F, 0.15F));
+    align->set_alignment(GUI::Alignment::Center);
+    align->set_width_policy(GUI::SizePolicy::Fill);
+    align->set_preferred_height(GUI::Length::from_pixels(300));
+
+    group->set_widget_preferred_width(add_child(group), GUI::Length::from_pixels(100));
 
     window->show();
-    return application->execute();
+    app->execute();
+    return 0;
 }
