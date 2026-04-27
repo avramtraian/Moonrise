19c8dab8589f4e4d8f4d27c48c01736fd0d245f7
Traian Avram <traian.avram1@gmail.com>
2026-04-27 18:55:55 +0300

    AND: Added math utility functions for working with powers of two

--- AND/MathUtilities.h
+++ AND/MathUtilities.h
@@ -5,7 +5,8 @@
 
 #pragma once
 
-#include "AND/Types.h"
+#include <AND/Assertions.h>
+#include <AND/Types.h>
 
 namespace AND {
 
@@ -37,11 +38,31 @@ NODISCARD ALWAYS_INLINE constexpr T divide_int_up(T numerator, T denominator)
     return (numerator + denominator - 1) / denominator;
 }
 
+template<typename T>
+requires(is_integer<T>)
+NODISCARD ALWAYS_INLINE constexpr bool is_power_of_two(T value)
+{
+    return value > 0 && ((value & (value - 1)) == 0);
+}
+
+template<typename T>
+requires(is_integer<T>)
+NODISCARD ALWAYS_INLINE constexpr T round_up_to_power_of_two(T value, T power_of_two)
+{
+    if (!is_power_of_two(power_of_two))
+        PANIC("Trying to invoke 'round_up_to_power_of_two' with a non-power-of-two value!");
+
+    T mask = ~(power_of_two - 1);
+    return (value + mask) & mask;
+}
+
 } // namespace AND
 
 #if AND_INCLUDE_IN_GLOBAL_NAMESPACE
 using AND::clamp;
 using AND::divide_int_up;
+using AND::is_power_of_two;
 using AND::max;
 using AND::min;
+using AND::round_up_to_power_of_two;
 #endif
